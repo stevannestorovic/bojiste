0107711afe35bb5e8471e7849b2b3d5cf03aef1d
stevan <nestorovicstevan@tutanota.de>
2019-12-02 16:46:49 +0000

    Rad na izvestaju o porudzbini.

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,8 +1,9 @@
+console.log('checkoutjs');
 simpleCart.ready(function() {
   simpleCart.each(function(item, x) {
-    $('#checkoutForm').append('<input  type="hidden" name="'  + ' Name" value="' + item.get('name') + '">');
-    $('#checkoutForm').append('<input  type="hidden" name="'  + ' Cena" value="' + item.get('price') + '">');
-    $('#checkoutForm').append('<input  type="hidden" name="'  + ' Kol" value="' + item.get('quantity') + '">');
+    $('#checkoutForm').append('<input  type="hidden" name="'  + 'Artikal: " value="' + item.get('name') + 'x' + item.get('quantity')+'">');
+   // $('#checkoutForm').append('<input  type="hidden" name="'  + ' Cena" value="' + item.get('price') + '">');
+   // $('#checkoutForm').append('<input  type="hidden" name="'  + ' Kol" value="' + item.get('quantity') + '">');
     console.log(document.querySelector('#checkoutForm'));
   });
 });
